6d68a920ad29f726963ed6b68607e367dc0a6e8f
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-07-10 16:18:09 -0300

    cadastro e repository

---
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -0,0 +1,31 @@
+package com.example.convidados.service.repository
+
+import com.example.convidados.service.model.GuestModel
+import java.util.ArrayList
+
+class GuestRepository {
+
+    fun getAll(): List<GuestModel> {
+        val list: MutableList<GuestModel> = ArrayList()
+        return list
+    }
+
+    fun getPresent(): List<GuestModel> {
+        val list: MutableList<GuestModel> = ArrayList()
+        return list
+    }
+
+    fun getAbsent(): List<GuestModel> {
+        val list: MutableList<GuestModel> = ArrayList()
+        return list
+    }
+
+    fun update(guest: GuestModel){
+    }
+
+    fun delete(guest: GuestModel){
+    }
+
+    fun save(guest: GuestModel) {
+    }
+}
--- app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
@@ -1,8 +1,21 @@
 package com.example.convidados.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.convidados.service.model.GuestModel
+import com.example.convidados.service.repository.GuestRepository
 
 class GuestFormViewModel : ViewModel(){
 
+    private val mGuestRepository: GuestRepository = GuestRepository()
+
+    private var mSaveGuest = MutableLiveData<Boolean>() // MutableLiveData o valor pode ser modificado
+    val saveGuest: LiveData<Boolean> = mSaveGuest // LiveData o valor não pode ser modificado
+
+    fun save (name: String, presence: Boolean) {
+        val guest = GuestModel(name, presence)
+        mGuestRepository.save(guest)
+    }
 
 }
