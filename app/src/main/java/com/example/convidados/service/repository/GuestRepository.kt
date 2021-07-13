434490c88a43502b1cca1afa5c3e054bd94c2877
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-07-13 10:47:45 -0300

    implementando padrao singleton banco de dados

--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,9 +1,27 @@
 package com.example.convidados.service.repository
 
+import android.content.Context
 import com.example.convidados.service.model.GuestModel
 import java.util.ArrayList
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object { // singleton
+        private lateinit var repository: GuestRepository
+
+        fun getInstance (context: Context) : GuestRepository {
+            if(!::repository.isInitialized){
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
+    fun save(guest: GuestModel) {
+        
+    }
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
@@ -26,6 +44,4 @@ class GuestRepository {
     fun delete(guest: GuestModel){
     }
 
-    fun save(guest: GuestModel) {
-    }
 }
